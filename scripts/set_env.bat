3f9b3231ca2e86cde79566b3bd4d60663017493e
johnhungerford <hungerfordjustice@gmail.com>
2022-04-13 19:43:33 -0400

    Fixed init batch

--- scripts/set_env.bat
+++ scripts/set_env.bat
@@ -1,4 +1,8 @@
+@echo off
+
+set parent=%~dp0
+for %%B in (%parent%.) do set grandparent=%%~dpB
 pip install -r requirements.txt
 python -m venv .venv
+echo set PYTHONPATH=%grandparent% >> .venv\Scripts\activate.bat
 .venv\Scripts\activate.bat
-set PYTHONPATH=%cd%
