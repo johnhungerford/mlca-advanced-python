fbf63f56263408f442e15381c2cc775616ce00ac
johnhungerford <hungerfordjustice@gmail.com>
2022-04-27 22:09:07 -0400

    Updated classwork

---
+++ ronan_solutions/find_value_index_impl.py
@@ -0,0 +1,13 @@
+from typing import List
+
+from algorithms.lists.find_value_index import test_find_value_index
+
+
+def find_value_index(nums: List[int], target: int) -> int:
+    """This function searches a *sorted* list of numbers (ascending order) for a
+       target value. If it finds the value it returns its index. If it does not
+       find the value, it returns the index where that value would be if it were in the list"""
+    pass
+
+
+test_find_value_index.run_on(find_value_index)
