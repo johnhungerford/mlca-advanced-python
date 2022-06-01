6f3dbd3b59eafff34573ce196b012e0192e13a63
6Pinecones <103605411+6Pinecones@users.noreply.github.com>
2022-06-01 16:39:24 -0400

    jun1_2

--- physics_engine/physics_system.py
+++ physics_engine/physics_system.py
@@ -20,4 +20,4 @@ class PhysicsSystem:
     def update_state(self, delta_t: float):
         for physics_object in self.physics_objects:
             for physics_field in self.physics_fields: 
-                physics_object.update_state(delta_t, physics_field.force_x(physics_field))
+                physics_object.update_state(delta_t, physics_field.force_x(physics_object))
