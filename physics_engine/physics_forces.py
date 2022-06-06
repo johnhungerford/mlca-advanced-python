42284cd85e21cf0a866b17ad88f37c9bdf5091f5
johnhungerford <hungerfordjustice@gmail.com>
2022-06-06 13:10:37 -0400

    Week 5 prep (#18)

--- physics_engine/physics_forces.py
+++ physics_engine/physics_forces.py
@@ -34,3 +34,62 @@ class GravitationalPhysicsField(PhysicsField):
 
     def force_x(self, physics_object: PhysicsObject) -> float:
         return self.force_x_per_mass / physics_object.mass
+
+
+class PhysicsInteraction(abc.ABC):
+    """
+    Represents an interaction between two physics objects
+    """
+
+    @abc.abstractmethod
+    def can_interact(self, obj_1: PhysicsObject, obj_2: PhysicsObject) -> bool:
+        """
+        Return true if there is or can be a force between the two objects. Used to filter out interactions
+        where no force exists
+        """
+
+    @abc.abstractmethod
+    def force_x(self, acted_upon: PhysicsObject, actor: PhysicsObject) -> float:
+        """
+        "acted_upon" is the object that undergoes the force in question. "actor" is the object whose
+        presence causes the force on "acted_upon". Returns the force produced on "acted_upon" by the interaction.
+        Note that Newton's 3rd law, which states that force every force there exists an equal and opposite force,
+        we can take the opposite of this force (i.e., multiply it by -1) to get the force on "actor," which will
+        allow us to avoid calculating the value twice.
+        """
+
+
+class ElectricChargeInteraction(PhysicsInteraction):
+    """
+    Represents an interaction between charged particles
+    """
+
+    def __init__(self, force_charge_constant: float):
+        self.force_charge_constant: float = force_charge_constant
+
+    def can_interact(self, obj_1: PhysicsObject, obj_2: PhysicsObject) -> bool:
+        """They can only interact if both have charge"""
+        return 'charge' in obj_1.properties and 'charge' in obj_2.properties
+
+    def force_x(self, acted_upon: PhysicsObject, actor: PhysicsObject) -> float:
+        """Force between two charge particles is -charge_1 * charge_2 / (distance ^ 2) multipled by some constant"""
+        distance = acted_upon.pos_x - actor.pos_x
+        return - self.force_charge_constant * (acted_upon.properties['charge'] * actor.properties['charge']) / (distance * distance)
+
+
+class GravitationalInteraction(PhysicsInteraction):
+    """
+    Represents the gravitational force between any two objects (with mass)
+    """
+
+    def __init__(self, force_mass_constant: float):
+        self.force_mass_constant: float = force_mass_constant
+
+    def can_interact(self, obj_1: PhysicsObject, obj_2: PhysicsObject) -> bool:
+        """Since all objects have mass, all objects will interact"""
+        return True
+
+    def force_x(self, acted_upon: PhysicsObject, actor: PhysicsObject) -> float:
+        """Force between two objects is mass_1 * mass_2 / (distance ^ 2) multipled by some constant"""
+        distance = acted_upon.pos_x - actor.pos_x
+        return self.force_mass_constant * (acted_upon.mass * actor.mass) / (distance * distance)
--- physics_engine/physics_object_test.py
+++ physics_engine/physics_object_test.py
@@ -42,24 +42,24 @@ def test_position_should_update_by_velocity_times_delta_t_if_there_is_force():
     assert (actual_pos_x_1 == expected_pos_x_1)
 
 
-# def acceleration_from_force_and_mass_should_correctly_calculate_acceleration():
-#     test_mass = 2
-#     test_force = 50
-#     expected_acceleration = 25
-#     assert(acceleration_from_force_and_mass(test_force, test_mass) == expected_acceleration)
-#
-#
-# def test_velocity_should_update_by_force_divided_by_mass_times_delta_t_if_there_is_force():
-#     test_mass = 10
-#     test_pos_x_0 = 0
-#     test_vel_x_0 = 5
-#     test_force_x_0 = 100
-#     test_delta_t = 5
-#     expected_pos_x_1 = 25  # delt_t * vel_x_0 + pos_x_0
-#     expected_vel_x_1 = 50  # delta_t * (force / mass) + vel_x_0
-#     test_obj = PhysicsObject(test_pos_x_0, test_vel_x_0, test_mass)
-#     test_obj.update_state(test_delta_t, test_force_x_0)
-#     actual_pos_x_1 = test_obj.pos_x
-#     actual_vel_x_1 = test_obj.vel_x
-#     assert (actual_pos_x_1 == expected_pos_x_1)
-#     assert (actual_vel_x_1 == expected_vel_x_1)
+def acceleration_from_force_and_mass_should_correctly_calculate_acceleration():
+    test_mass = 2
+    test_force = 50
+    expected_acceleration = 25
+    assert(acceleration_from_force_and_mass(test_force, test_mass) == expected_acceleration)
+
+
+def test_velocity_should_update_by_force_divided_by_mass_times_delta_t_if_there_is_force():
+    test_mass = 10
+    test_pos_x_0 = 0
+    test_vel_x_0 = 5
+    test_force_x_0 = 100
+    test_delta_t = 5
+    expected_pos_x_1 = 25  # delt_t * vel_x_0 + pos_x_0
+    expected_vel_x_1 = 55  # delta_t * (force / mass) + vel_x_0
+    test_obj = PhysicsObject(test_pos_x_0, test_vel_x_0, test_mass)
+    test_obj.update_state(test_delta_t, test_force_x_0)
+    actual_pos_x_1 = test_obj.pos_x
+    actual_vel_x_1 = test_obj.vel_x
+    assert (actual_pos_x_1 == expected_pos_x_1)
+    assert (actual_vel_x_1 == expected_vel_x_1)
