3810e71fe902e367eaa238cd027da93f51f9dc22
Marshall Zafiriou <marshallzafiriou427@gmail.com>
2022-06-08 07:42:56 -0400

    the renderer and other stuff

--- physics_engine/examples.py
+++ physics_engine/examples.py
@@ -4,8 +4,9 @@ from physics_engine.physics_object import PhysicsObject
 from physics_engine.physics_system import PhysicsSystem
 from physics_engine.system_renderer import Ascii1DPhysicsSystemRenderer
 
-physics_system: PhysicsSystem = None # PUT SOMETHING HERE!
+physics_system: PhysicsSystem = x # PUT SOMETHING HERE!
 physics_renderer = Ascii1DPhysicsSystemRenderer(physics_system)
 
 if __name__ == "__main__":
+    obj1 = PhysicsObject(5, 1, 4)
     physics_renderer.render_continually()
