3f8507a88a1aa0519712960dcf2d8909b290442d
Ronan M <Ronanscottm@gmail.com>
2022-06-08 16:24:20 -0400

    '423'

--- physics_engine/physics_system.py
+++ physics_engine/physics_system.py
@@ -2,6 +2,7 @@ from typing import List
 
 from physics_engine.physics_forces import PhysicsField
 from physics_engine.physics_object import PhysicsObject
+from physics_engine.physics_forces import PhysicsInteraction
 
 
 class PhysicsSystem:
@@ -13,11 +14,21 @@ class PhysicsSystem:
 
     Now that we are adding
     """
-    def __init__(self, physics_objects: List[PhysicsObject], physics_fields: List[PhysicsField]):
+    def __init__(self, physics_objects: List[PhysicsObject], physics_fields: List[PhysicsField], physics_interactions: List[PhysicsInteraction]):
         self.physics_objects: List[PhysicsObject] = physics_objects
         self.physics_fields: List[PhysicsField] = physics_fields
+        self.physics_interactions: List[PhysicsInteraction] = physics_interactions
     
     def update_state(self, delta_t: float):
         for physics_object in self.physics_objects:
+            net_force = 0
+
             for physics_field in self.physics_fields: 
-                physics_object.update_state(delta_t, physics_field.force_x(physics_object))
+                net_force += physics_field.force_x(physics_object)
+            
+            for physics_interaction in self.physics_interactions:
+                for other_object in self.physics_objects:
+                    if physics_object != other_object:
+                        net_force += physics_interaction.force_x(physics_object, other_object)
+            
+            physics_object.update_state(delta_t, net_force)
